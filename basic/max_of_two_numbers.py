d4bed65894fff8d6675222d3a78a224cffca03b8
SoumyaM <soumyamishra263@gmail.com>
2026-06-06 13:16:11 +0530

    feat(basic): max of two numbers program

---
+++ basic/max_of_two_numbers.py
@@ -0,0 +1,18 @@
+# Find the maximum of two numbers
+
+
+
+def max_of_two_numbers(num1, num2):
+    max_num = None
+    if num1 > num2:
+        max_num = num1
+    else:
+        max_num = num2
+    print(f"The max number is: {max_num}")
+
+max_of_two_numbers(12,13)
+
+
+
+
+
