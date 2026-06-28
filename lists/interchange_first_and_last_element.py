b6d2a4642c50d8e8bea2a3d5b47fef3de0360c7c
SoumyaM <soumyamishra263@gmail.com>
2026-06-28 13:48:34 +0530

    feat(lists): Interchange first and last elements using different methods

--- lists/interchange_first_and_last_element.py
+++ lists/interchange_first_and_last_element.py
@@ -1,10 +1,26 @@
 # Program to interchange the first and last elements in a list
 
 def interchange_elements(arr_list):
-    temp = arr_list[0]
-    arr_list[0] = arr_list[-1]
-    arr_list[-1] = temp
+    # Using Temporary Variable
+    # temp = arr_list[0]
+    # arr_list[0] = arr_list[-1]
+    # arr_list[-1] = temp
+    # print(f"The updated array list using Temporary Variable {arr_list}")
 
-    print(f"The updated array list is {arr_list}")
+    # Using Direct Assignment
+    arr_list[0], arr_list[-1] = arr_list[-1], arr_list[0]
+    print(f"The updated array list using Direct Assignment method {arr_list}")
+
+    # Using Tuple Variable
+    # pair = arr_list[-1], arr_list[0] # pair = (21,2)
+    # arr_list[0], arr_list[-1] = pair
+    # print(f"The updated array list using Tuple Variable {arr_list}")
+
+    # Using Slicing
+    firstElem = arr_list[:1]
+    lastElem = arr_list[-1:]
+    midElements = arr_list[1:-1] # elements between first and last
+    arr_list = lastElem + midElements + firstElem
+    # print(f"The updated array list through slicing is {arr_list}")
 
 interchange_elements(arr_list = [2,5,7,9,11,13,19,20,21])
