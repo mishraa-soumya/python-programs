ff74b9e83c70e0b0cd8ceeb4c2abb8a5d8cdc57c
SoumyaM <soumyamishra263@gmail.com>
2026-07-04 21:52:32 +0530

    feat(lists): program to add different ways to reverse a list

--- lists/reversing_a_list.py
+++ lists/reversing_a_list.py
@@ -11,6 +11,22 @@ def reversing_a_list_using_loop(arr):
 
     print(f"The reverse list is {arr}")
 
+def reversing_a_list_using_reverse_method():
+    arr = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+    arr.reverse()
+    print(f"The reverse array using reverse method: {arr}")
+
+def reverse_using_list_slicing():
+    arr = [2, 3, 4, 5, 6, 7]
+    rev_arr = arr[::-1] # double colon with negative Index reverse the list
+    print(f"The reverse array using list slicing: {rev_arr}")
+
 def reversing_a_list(arr = [2, 3, 4, 5, 6, 7, 8, 9, 10]):
     # Using a loop
     reversing_a_list_using_loop(arr)
+    # Using the reverse method
+    reversing_a_list_using_reverse_method()
+    # Using list slicing
+    reverse_using_list_slicing()
+
+reversing_a_list()
