9a9137776a9f435a6c92bf9a0d6d7b1066b6d76b
SoumyaM <soumyamishra263@gmail.com>
2026-07-28 18:24:51 +0530

    feat(lists): program to add two matrices using list comprehension

--- matrix/add_two_matrices.py
+++ matrix/add_two_matrices.py
@@ -2,12 +2,28 @@
 
 import numpy as np
 
-def matrix_addition():
+def matrix_addition_using_numpy():
     matrix1 = np.array([[1,2,3], [4, 5, 6], [7, 8, 9]])
     matrix2 = np.array([[9, 8, 7], [6, 5, 4], [3, 2, 1]])
     result = matrix1 + matrix2
 
     print(f"The matrix addition result is : {result}")
+
+def matrix_addition_using_list_comprehension():
+    matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    matrix2 = [[9, 8, 7], [6, 5, 4], [3, 2, 1]]
+    output = []
+    result = [[matrix1[i][j] + matrix2[i][j] for j in range(len(matrix1[0]))] for i in range(len(matrix1))]
+    for res in result:
+        output.append(res)
+    print(f"The result of matrix addition through list comprehensions is: {output}")
+
+
+def matrix_addition():
+    # Using Numpy
+    matrix_addition_using_numpy()
+    # Using List Comprehensions
+    matrix_addition_using_list_comprehension()
 
 # Main Program Calling
 matrix_addition()
