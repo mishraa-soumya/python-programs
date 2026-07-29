96399fa142d62b29bc251f53d296d7967c977e36
SoumyaM <soumyamishra263@gmail.com>
2026-07-29 21:00:07 +0530

    feat(matrix): Matrix addition using dynamic params

--- matrix/add_two_matrices.py
+++ matrix/add_two_matrices.py
@@ -2,16 +2,16 @@
 
 import numpy as np
 
-def matrix_addition_using_numpy():
-    matrix1 = np.array([[1,2,3], [4, 5, 6], [7, 8, 9]])
-    matrix2 = np.array([[9, 8, 7], [6, 5, 4], [3, 2, 1]])
+def matrix_addition_using_numpy(mx1, mx2):
+    matrix1 = np.array(mx1)
+    matrix2 = np.array(mx2)
     result = matrix1 + matrix2
 
     print(f"The matrix addition result is : {result}")
 
-def matrix_addition_using_list_comprehension():
-    matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    matrix2 = [[9, 8, 7], [6, 5, 4], [3, 2, 1]]
+def matrix_addition_using_list_comprehension(matrix1, matrix2):
+    # matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    # matrix2 = [[9, 8, 7], [6, 5, 4], [3, 2, 1]]
     output = []
     result = [[matrix1[i][j] + matrix2[i][j] for j in range(len(matrix1[0]))] for i in range(len(matrix1))]
     for res in result:
@@ -19,11 +19,11 @@ def matrix_addition_using_list_comprehension():
     print(f"The result of matrix addition through list comprehensions is: {output}")
 
 
-def matrix_addition():
+def matrix_addition(mx1, mx2):
     # Using Numpy
-    matrix_addition_using_numpy()
+    matrix_addition_using_numpy(mx1, mx2)
     # Using List Comprehensions
-    matrix_addition_using_list_comprehension()
+    matrix_addition_using_list_comprehension(mx1, mx2)
 
 # Main Program Calling
-matrix_addition()
+matrix_addition([[9,2,5], [2, 4, 7], [3, 6, 9]], [[9, 8, 7], [6, 5, 4], [3, 2, 1]])
