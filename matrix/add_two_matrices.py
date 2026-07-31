a53cf494b4ebd7f7518c9d2aaef077fca093672a
SoumyaM <soumyamishra263@gmail.com>
2026-07-31 16:42:21 +0530

    feat(lists): updated the program to multiply 2*2 matrix

--- matrix/add_two_matrices.py
+++ matrix/add_two_matrices.py
@@ -26,4 +26,4 @@ def matrix_addition(mx1, mx2):
     matrix_addition_using_list_comprehension(mx1, mx2)
 
 # Main Program Calling
-matrix_addition([[9,2,5], [2, 4, 7], [3, 6, 9]], [[9, 8, 7], [6, 5, 4], [3, 2, 1]])
+matrix_addition([[9,2], [2, 4], [3, 6]], [[9, 8], [6, 5], [3, 2]])
