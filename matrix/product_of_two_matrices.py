ec8a22ddec427c02976581024e64490acd3c4e64
SoumyaM <soumyamishra263@gmail.com>
2026-08-09 21:23:22 +0530

    feat(matrix): Program to find product of the matrix using math module

--- matrix/product_of_two_matrices.py
+++ matrix/product_of_two_matrices.py
@@ -2,6 +2,8 @@
 # Example: [[1, 4, 5], [7, 3], [4], [46, 7, 3]]
 
 import numpy as np
+import math
+
 def product_of_matrix_using_numpy(mx):
     # flatten the matrix to a 1-D List
     flattened_list = [ele for sub in mx for ele in sub] # Using List Compression to flattened the matrix before getting the product
@@ -9,9 +11,19 @@ def product_of_matrix_using_numpy(mx):
 
     print(f"The product of matrix is: {matrix_product}")
 
+def product_of_matrix_using_math(matriqs):
+    # flattened the matrix to a 1-D List
+    flattened_list = [elm for sub in matriqs for elm in sub]
+    matrix_product = math.prod(flattened_list)
+
+    print(f"The product of matrix using math is: {matrix_product}")
+
 def product_of_matrix(matriqs):
     # Using numpy.prod to find out the product of the matrix
     product_of_matrix_using_numpy(matriqs)
+    # Using math.prod to find out the product of the matrix
+    product_of_matrix_using_math(matriqs)
+
 
 # calling main program
 matriqs = [[1, 4, 5], [7, 3], [4], [46, 7, 3]]
