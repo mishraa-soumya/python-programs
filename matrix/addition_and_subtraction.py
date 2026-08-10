d85e186f8280ce75f5e0ec1cf93b7bb60f61f934
SoumyaM <soumyamishra263@gmail.com>
2026-08-10 20:47:31 +0530

    feat(matrix): Program to subtract the matrices using numpy.subtract

--- matrix/addition_and_subtraction.py
+++ matrix/addition_and_subtraction.py
@@ -8,3 +8,10 @@ def adding_two_matrices(mtr1, mtr2):
     print(f"The matrix addition using 'numpy.add': {result}")
 
 adding_two_matrices([[1, 2], [3, 4]], [[4, 5], [6, 7]])
+
+def subtract_two_matrices(mx1, mx2):
+    # subtract method performs subtraction of matrices element wise
+    finalMatrix = np.subtract(mx1, mx2)
+    print(f"Result of matrix subtraction using 'numpy.subtract': {finalMatrix}")
+
+subtract_two_matrices([[9, 7], [8, 12]], [[4, 5], [6, 7]])
