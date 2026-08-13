95828a5aa34475bfe1980ebabe3993b74fc85efd
SoumyaM <soumyamishra263@gmail.com>
2026-08-13 13:27:03 +0530

    feat(matrix): Program to create a n*n custom value matrix

--- matrix/n_n_matrix_creation.py
+++ matrix/n_n_matrix_creation.py
@@ -5,8 +5,17 @@ def create_zero_matrix(n):
     result = np.zeros((n,n), dtype=int)
     print(f"The final matrix of dimension {n} is {result}")
 
-def create_matrix(n, type = 'zero'):
+def create_custom_matrix(n, val):
+    result = np.full((n,n), val)
+    print(f"\n ======= \n The custom matrix of value: {val} and dimension: {n} is: {result}")
+
+def create_matrix(n, type = 'zero', val=6):
     if type == "zero":
         create_zero_matrix(n)
+    elif type == "full":
+        create_custom_matrix(n, val)
 
+# Calling the program to create Zero matrix
 create_matrix(4)
+# Calling the program to create a full matrix
+create_matrix(3, 'full',8)
