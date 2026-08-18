af60b2c0940e1751826736f9bb0bcb72b7188d04
SoumyaM <soumyamishra263@gmail.com>
2026-08-18 12:15:01 +0530

    feat(matrix): program to concatenate the column data for any matrix using pandas library

---
+++ matrix/vertical_concatenation_of_matrices.py
@@ -0,0 +1,24 @@
+# Problem: Given a matrix containing strings, the task is to perform vertical concatenation where elements from each columnare joined together to form a single string for that column.
+
+# Example:
+# Input: [["Gfg", "good"], ["is", "for"]]
+# Output: [["Gfgis", "goodfor"]]
+
+import pandas as pd
+
+def vc_using_pandas(mat):
+    # Create a data frame from the matrix using pandas
+    data_frame = pd.DataFrame(mat)
+    # Replace all the missing values with empty strings and join the strings of each column
+    column_data = data_frame.fillna(" ").apply("".join)
+    # Convert the column data back into a list form
+    result = list(column_data)
+    print(f"Result of vertical concatenation of a matrix: {result}")
+
+def vertical_concatenation_of_matrix(mat):
+    # Using Pandas to concatenation
+    vc_using_pandas(mat)
+
+# Main Program
+mat = [["Python", "good"], ["is", "for"], ["Best"]]
+vertical_concatenation_of_matrix(mat)
