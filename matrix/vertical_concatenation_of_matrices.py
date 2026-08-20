a616baadeb95b11d71f85adcd081d6a7b8b8506f
SoumyaM <soumyamishra263@gmail.com>
2026-08-20 21:32:54 +0530

    feat(matrix):Program to vertical concatenation of a matrix using numpy

--- matrix/vertical_concatenation_of_matrices.py
+++ matrix/vertical_concatenation_of_matrices.py
@@ -5,7 +5,7 @@
 # Output: [["Gfgis", "goodfor"]]
 
 import pandas as pd
-
+import numpy as np
 def vc_using_pandas(mat):
     # Create a data frame from the matrix using pandas
     data_frame = pd.DataFrame(mat)
@@ -15,9 +15,20 @@ def vc_using_pandas(mat):
     result = list(column_data)
     print(f"Result of vertical concatenation of a matrix: {result}")
 
+# Using Numpy
+def vc_using_numpy(mat):
+    max_column = max(len(x) for x in mat) # Finds the maximum sublist or the max column
+    p = [x + [''] * (max_column - len(x)) for x in mat]
+    arr = np.array(p).T
+    res = [''.join(r) for r in arr]
+    print(f"Result of vertical concatenation using Numpy: {str(res)}")
+
+
 def vertical_concatenation_of_matrix(mat):
     # Using Pandas to concatenation
     vc_using_pandas(mat)
+    # Using Numpy for concatenation
+    vc_using_numpy(mat)
 
 # Main Program
 mat = [["Python", "good"], ["is", "for"], ["Best"]]
